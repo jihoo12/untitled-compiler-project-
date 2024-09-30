0e3b1968d6098c8c4b82a03c060a15ed47b18675
jihoo <134384139+jihoo12@users.noreply.github.com>
2024-09-30 03:10:51 +0000

    main.rs update

--- backend/main.rs
+++ backend/main.rs
@@ -21,19 +21,97 @@ fn findprefix(s: &String) -> usize {
     s.len()
 }
 fn ir(s: &String) {
+    //i will add error handling 
     let word = first_word(s);
     let start_word = &s[..word];
     let colon = findprefix(s);
     let prefix1 = &start_word[..colon];
     let prefix2 = &start_word[colon+1..];
     match prefix1 {
-         "s" => {},
-         "n" => {},
-         "i" => {},
-         "r" => {},
-         "r+i" => {},
-         "r-i" => {},
-         "m" => {},
+         "s" => {
+             match prefix2 {
+                "s" => {},
+                "n" => {},
+                "i" => {},
+                "r" => {},
+                "r+i" => {},
+                "r-i" => {},
+                "m" => {},
+                _ => {}, //error 
+             }
+         },
+         "n" => {
+             match prefix2 {
+                "s" => {},
+                "n" => {},
+                "i" => {},
+                "r" => {},
+                "r+i" => {},
+                "r-i" => {},
+                "m" => {},
+                _ => {}, //error 
+             }
+         },
+         "i" => {
+             match prefix2 {
+                 "s" => {},
+                 "n" => {},
+                 "i" => {},
+                 "r" => {},
+                 "r+i" => {},
+                 "r-i" => {},
+                 "m" => {},
+                 _ => {}, //error 
+             }
+         },
+         "r" => {
+             match prefix2 {
+                 "s" => {},
+                 "n" => {},
+                 "i" => {},
+                 "r" => {},
+                 "r+i" => {},
+                 "r-i" => {},
+                 "m" => {},
+                 _ => {}, //error 
+             } 
+         },
+         "r+i" => {
+             match prefix2 {
+                 "s" => {},
+                 "n" => {},
+                 "i" => {},
+                 "r" => {},
+                 "r+i" => {},
+                 "r-i" => {},
+                 "m" => {},
+                 _ => {}, //error 
+            }
+         },
+         "r-i" => {
+             match prefix2 {
+                 "s" => {},
+                 "n" => {},
+                 "i" => {},
+                 "r" => {},
+                 "r+i" => {},
+                 "r-i" => {},
+                 "m" => {},
+                 _ => {}, //error 
+             }
+         },
+         "m" => {
+                 match prefix2 {
+                     "s" => {},
+                     "n" => {},
+                     "i" => {},
+                     "r" => {},
+                     "r+i" => {},
+                     "r-i" => {},
+                     "m" => {},
+                     _ => {}, //error 
+             }
+         },
          _ => {}, //error 
     }
 } 
