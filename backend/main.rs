83fda2c56e168e09816c222abe4855192b7094ef
jihoo <134384139+jihoo12@users.noreply.github.com>
2024-09-30 02:28:52 +0000

    main.rs update

--- backend/main.rs
+++ backend/main.rs
@@ -1,3 +1,14 @@
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
 fn ir(s: &String) {
     let word = first_word(s);
     let start_word = s[..word];
