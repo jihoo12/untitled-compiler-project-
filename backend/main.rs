3b08f01f84432aa5e6d82b4b59cf026f0d250e53
jihoo <134384139+jihoo12@users.noreply.github.com>
2024-09-30 02:33:40 +0000

    main.rs update

--- backend/main.rs
+++ backend/main.rs
@@ -9,7 +9,21 @@ fn first_word(s: &String) -> usize {
 
     s.len()
 }
+fn findprefix(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b',' {
+            return i;
+        }
+    }
+
+    s.len()
+}
 fn ir(s: &String) {
     let word = first_word(s);
     let start_word = s[..word];
+    let colon = findprefix(s);
+    let prefix1 = start_word[..colon]
+    let prefix2 = start_word[colon..]
 } 
