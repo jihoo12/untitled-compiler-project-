434dd8792c94acf440605a32b5dcd82f2b2f01cd
jihoo <134384139+jihoo12@users.noreply.github.com>
2024-10-02 04:22:07 +0000

    main.rs update

--- backend/main.rs
+++ backend/main.rs
@@ -18,6 +18,7 @@ fn ir(prefix1: &str, prefix2: &str, command: &str,args: IrArgs) {
              //only register available 
              // 48 89 
              // 00 000(reg) 100 00(scale) 000(index) 000(base)
+             // 04+8*reg+4*space+8*reg+reg
              match command {
                 "mov" => {
                     if (args.isdisp == true) {
